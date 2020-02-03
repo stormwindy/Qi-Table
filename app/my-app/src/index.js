7823501e84a40a968b1a30e2af1fd1128c3c95f8
Maciek <maciek@c119112.wlan.net.ed.ac.uk>
2020-02-03 11:52:25 +0000

    Added App Skeleton

---
+++ app/my-app/src/index.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+// var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+
+class MainView extends React.Component{  
+    constructor(props){
+        super(props);
+        this.state = {
+            view: "select"
+        }
+    }
+
+    render(){
+        let view;
+        if (this.state.view === "select") view = <SelectionView />;
+        if (this.state.view === "editor") view = <EditorView />;
+        if (this.state.view === "settings") view = <SettingsView />;
+
+        return (
+            <div>
+                <ul>
+                    <li><button onClick={() => this.setState({view: "select"})}>Selection view</button></li>
+                    <li><button onClick={() => this.setState({view: "editor"})}>Editor view</button></li>
+                    <li><button onClick={() => this.setState({view: "settings"})}>Settings view</button></li>
+                </ul>
+                {view}
+            </div>
+        );
+    }
+}
+
+class SelectionView extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>This is the selection view. Choose a layout.</h1>
+                <ul>
+                    <li><LayoutCard name={1}/></li>
+                    <li><LayoutCard name={2}/></li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+class EditorView extends React.Component{
+    render(){
+        return(
+            <div><h1>This is the editor view. Create a new layout.</h1></div>
+        );
+    }
+}
+
+class SettingsView extends React.Component{
+    render(){
+        return(
+            <div><h1>This is the settings view. Change settings here.</h1></div>
+        );
+    }
+}
+
+class LayoutCard extends React.Component{
+    render(){
+        return(
+            <div>
+            <p>Layout {this.props.name}</p>
+            <button onClick={() => alert("Moving into layout " + this.props.name + "...")}>Choose this layout!</button>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<MainView />, document.getElementById('root'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ env_qi/lib/python3.7/_dummy_thread.py
@@ -0,0 +1,1 @@
+/Users/maciek/anaconda3/lib/python3.7/_dummy_thread.py
