a7e4c5f312cb01d0014c9adcf87f5e4de3d44894
smiltainis <smilte.petronyte@gmail.com>
2020-02-18 00:04:41 +0000

    updated draft

--- server/pathfinding/field_path_gh.py
+++ server/pathfinding/field_path_gh.py
@@ -15,6 +15,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from .table_functions import *
 
+import sys
+sys.path.append("..\\..\\")
+from base import Table, Point2, Rectangle
+# from server.vision.camera import Camera
+
 # Parameters
 KP = 5.0  # attractive potential gain
 ETA = 100.0  # repulsive potential gain
@@ -41,6 +46,7 @@ def calc_potential_field(gx, gy, ox, oy, reso, rr):
 
     # calc each potential
     pmap = [[0.0 for i in range(yw)] for i in range(xw)]
+
 
     for ix in range(xw):
         x = ix * reso
@@ -99,6 +105,27 @@ def get_motion_model():
 
     return motion
 
+def my_get_motion_model():
+    # changed to a list of tuples in order to form a dictionary
+    # dx, dy
+    motion = [(1, 0),
+              (0, 1),
+              (-1, 0),
+              (0, -1),
+              (-1, -1),
+              (-1, 1),
+              (1, -1),
+              (1, 1)]
+
+    # direction of movements depending on which motion is selected for the next step
+    # probably will need to change values!!! also values depend on the reference frame we will be using
+    # I have selected these assuming angle zero means the table's width is parallel to the x axis
+    # and positive rotation is clockwise;
+    # however it would be better if these match with the orientation of the table
+    angles = [90.0, 0.0, 0.0, 90.0, -45.0, 45.0, 45.0, -45.0]
+    motion_angles = dict(zip(motion, angles))
+
+    return motion, motion_angles
 
 def potential_field_planning(sx, sy, gx, gy, ox, oy, reso, rr):
 
@@ -130,6 +157,7 @@ def potential_field_planning(sx, sy, gx, gy, ox, oy, reso, rr):
         for i, _ in enumerate(motion):
             inx = int(ix + motion[i][0])
             iny = int(iy + motion[i][1])
+            # do we need to check if it is not out of the lower bounds too????
             if inx >= len(pmap) or iny >= len(pmap[0]):
                 p = float("inf")  # outside area
             else:
@@ -155,83 +183,105 @@ def potential_field_planning(sx, sy, gx, gy, ox, oy, reso, rr):
 
     return rx, ry
 
+# sx = current x position of table centre
+# sy = current y position of table centre
+def calc_potential_field_for_table(sx, sy, gx, gy, ox, oy, rr):
 
+    # create 3 x 3 matrix to only represent the gird cells around the table centre position
+    pmap = np.zeros(3, 3)
+    for x in range(3):
+        for y in range(3):
+            ug = calc_attractive_potential(sx, sy, gx, gy)
+            uo = calc_repulsive_potential(sx, sy, ox, oy, rr)
+            uf = ug + uo
+            pmap[x][y] = uf
 
-def my_potential_field_planning(table, gx, gy, ox, oy, reso, rr):
+    return pmap
 
-    # calc potential field
-    pmap = calc_potential_field(gx, gy, ox, oy, reso, rr)
+def my_potential_field_planning(self, table, gx, gy, ox, oy, reso):
 
     sx = table.geometry.central_position.x
     sy = table.geometry.central_position.y
+    rr = diagonal(table) / 2.0
+
+    # adds room boundaries: table centre position can't come closer to the wall than its radius
+    grid_bound_left = int(round(rr / reso)) # number of cells the radius takes
+    grid_bound_right = int(round(ROOM_WIDTH / reso - rr / reso))
+    grid_bound_top = int(round(rr / reso))
+    grid_bound_bottom = int(round(ROOM_HEIGHT / reso - rr /reso))
 
     # search path
     d = np.hypot(sx - gx, sy - gy)
     ix = round(sx / reso)
     iy = round(sy / reso)
-    gix = round(gx / reso)
-    giy = round(gy / reso)
-
-    if show_animation:
-        draw_heatmap(pmap)
-        # for stopping simulation with the esc key.
-        plt.gcf().canvas.mpl_connect('key_release_event',
-                lambda event: [exit(0) if event.key == 'escape' else None])
-        plt.plot(ix, iy, "*k")
-        plt.plot(gix, giy, "*m")
-        # plt.show()
 
     rx, ry = [sx], [sy]
-    motion = get_motion_model()
+    motion, angles = my_get_motion_model()
 
     while d >= reso:
         minp = float("inf")
         minix, miniy = -1, -1
+        dir = (0, 0)
         for i, _ in enumerate(motion):
             inx = int(ix + motion[i][0])
             iny = int(iy + motion[i][1])
-            if inx >= len(pmap) or iny >= len(pmap[0]):
+            # calc potential field
+            pmap = calc_potential_field_for_table(sx, sy, ox, oy, rr)
+            if inx >= grid_bound_right or inx <= grid_bound_left \
+                    or iny >= grid_bound_bottom or iny <= grid_bound_top:
                 p = float("inf")  # outside area
             else:
-                p = pmap[inx][iny]
+                # pmap is 3x3 and the center of the table is in (1,1) thus need to calculate offset to get
+                # values in surrounding grid cells
+                offsetx = 1 - ix
+                offsety = 1 - iy
+                p = pmap[inx - offsetx][iny - offsety]
             if minp > p:
                 # indices of minimum potentials
                 minp = p
                 minix = inx
                 miniy = iny
+                # get the direction the robot needs to move to
+                dir = motion[i]
         ix = minix
         iy = miniy
         xp = ix * reso
         yp = iy * reso
 
-        # while
+        angle = angles[dir] #direction of movement
+        orientation = table.geometry.orientation
+
+        # id = table.table_id
+        #     # OR INSTEAD OF BELLOW while not (angle >= orientation - 1 and angle <= orientation + 1) : #added 1s for errors???
+        # while not angle == orientation :
+        #     #send direction to robot to rotate
+        #     pos = self.camera.get_pos(1)
+        #     ar1, ar2 = pos[id]
+        #     table = form_table(ar1, ar2)
+        #     orientation = table.geometry.orientation
+        #     would be good to have function get_orientation() probs easy to extract that part from form_table
+        #     orientation = get_orientation()
+
+        # table.orientation = angle # or new orientation
+
+        # while not (xp > sx - 0.5 and xp < sx + 0.5 and yp > sy - 0.5 and yp < sy + 0.5) :
+        #     send directions to the table to move forwards or backwards
+        #     pos = self.camera.get_pos(1)
+        #     ar1, ar2 = pos[id]
+        #     new_centre = table_centre(ar1, ar2)
+        #     sx = new_centre.x
+        #     sy = new_centre.y
+
+        # table.geometry.x = sx
+        # table.geometry.y = sy
 
         d = np.hypot(gx - xp, gy - yp)
         rx.append(xp)
         ry.append(yp)
 
-        if show_animation:
-            plt.plot(ix, iy, ".r")
-            plt.pause(0.01)
-
-    print("Goal!!")
-
+    # camera.release()
+    print (rx, ry)
     return rx, ry
-
-
-def move():
-
-    # pos = self.camera.get_pos(1) # to get table position from vision
-    # ar1, ar2 = pos[1]
-    # table = form_table(ar1, ar2, 1)
-
-    rectangle = Rectangle(5, 3, Point2(17.5, 38.5), 0, Point2(15, 40), Point2(15, 37), Point2(20, 40), Point2(20, 37))
-    table = Table(rectangle, 1)
-    # table radius
-    radius = diagonal(table) / 2.0
-
-
-    # self.camera.release()
 
 
 def draw_heatmap(data):
@@ -242,9 +292,9 @@ def draw_heatmap(data):
 def main():
     print("potential_field_planning start")
     #
-    sx = 5.0  # start x position [m]
+    sx = 5.3  # start x position [m]
     sy = 10.0  # start y positon [m]
-    gx = 30.0  # goal x position [m]
+    gx = 30.7  # goal x position [m]
     gy = 30.0  # goal y position [m]
     grid_size = 0.5  # potential grid size [m]
     robot_radius = 3.0  # robot radius [m]
