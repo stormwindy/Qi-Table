8d102604602f28e833261215e298276851d26082
Ege E <s1782480@sms.ed.ac.uk>
2020-02-20 13:22:19 +0000

    RF communication and NXT Motor Control Implemented (#27)
    * Add nxt_control script
    
    Added scripts and libraries allow LEGO NXT motors to be controlled using keyboard input. A simple function has been added to allow programmatic control of the motors. Check the source code for programmatic control details.
    
    * Changed function names to refactor as if it is a library file rather than main file.
    
    * Added test communication module code.
    
    * Added comments
    
    * Added library files for base communication module.
    
    * Added corrected comms files for base and Arduino.
    
    * Fixed some minor bugs. Current state of the program detects packets individually and returns them one by one.
    
    * Integrated all modules together. There are duplicates because Arduino IDE requires all libraries to be in the same sketch file. File structured will be cleaned when project is done.
    
    * eggfunctions added to nxt_control branch
    
    * Fixed a bug where baud rate was causing packets to be parsed wrong. Restructered the files.
    
    * Folder structure cleanup for pull request.
    
    Co-authored-by: Saad Sharif <44679743+saadsharif@users.noreply.github.com>

---
+++ hardware/Arduino/control/ArduinoControl/ArduinoControl.ino
@@ -0,0 +1,161 @@
+#include "SDPArduino.h"
+//#include "Comms.h"
+//#include "NxtControl.h"
+#include <Wire.h>
+#include <SoftwareSerial.h>
+#include <string.h>
+#define rxPin 10
+#define txPin 11
+
+const byte numChars = 3;
+char receivedChars[numChars]; // an array to store the received data
+char moveDirection = '5';
+char curDir = '5';
+bool newData = false;
+int lastIndex = 0;
+String commands = "012345";
+//SoftwareSerial ss(10, 11);
+//Comms comms;
+//NxtControl nxt(&ss);
+
+void setup()
+{
+  Serial.begin(9600);
+  SDPsetup();
+  delay(100);
+  delay(100);
+  //comms.setupComms();
+  //nxt.setupMotors();
+  Serial.println("Hello world");
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+}
+
+void loop()
+{
+  char commandChar = getCommand();
+  //comms.showNewData();
+  delay(200);
+  setDirection(commandChar, 75);
+  moveMotors();
+}
+
+char getCommand()
+{
+  recvWithEndMarker();
+  //if (packet == NULL) {return NULL;}
+  //showNewData();
+  newData = false;
+  return receivedChars[lastIndex - 1];
+}
+
+void recvWithEndMarker() 
+{
+  static byte ndx = 0;
+  char endMarker = '\n';
+  char rc;
+  
+  while (Serial.available() > 0 && newData == false) {
+    rc = Serial.read();
+    
+    if (rc != endMarker && commands.indexOf(rc) != -1 ) 
+    {
+      receivedChars[ndx] = rc;
+      ndx++;
+      if (ndx >= numChars) 
+      {
+        Serial.write("Exceeded packet size limit.");
+        ndx = 0;
+      }
+    }
+    else 
+    {
+      receivedChars[ndx] = '\0'; // terminate the string
+      lastIndex = ndx;
+      ndx = 0;
+      delay(100);
+      newData = true;
+    }
+  }
+}
+
+int power = 75;
+void setDirection(char dir, int powerInput)
+{
+  if (dir != moveDirection)
+  {
+    moveDirection = dir;
+  }
+  
+  if (power != powerInput)
+  {
+    power = powerInput; 
+  }
+  
+  //if (curDir == moveDirection) {return;}
+  curDir = moveDirection;
+  //Letter W/w. Move forward
+  if (moveDirection == '2')
+  {
+    motorForward(0, power);
+    motorForward(1, power);
+  } 
+  //Letter a/A. Left turn.
+  else if (moveDirection == '3')
+  {
+    motorForward(0, power);
+    motorBackward(1, power);
+  }
+  //Letter d/D. Right turn.
+  else if (moveDirection == '4')
+  {
+    motorBackward(0, power);
+    motorForward(1, power);
+  }
+  //Letter s/S. Rear movement
+  else if (moveDirection == '1')
+  {
+    motorBackward(0, power);
+    motorBackward(1, power);
+  }
+  //Letter q/Q. Stop movement.
+  else if (moveDirection == '5')
+  {
+    motorAllStop();
+  }
+}
+
+void moveMotors() 
+{
+  if (curDir == moveDirection) {return;}
+  curDir = moveDirection;
+  //Letter W/w. Move forward
+  if (moveDirection == '2')
+  {
+    motorForward(0, power);
+    motorForward(1, power);
+  } 
+  //Letter a/A. Left turn.
+  else if (moveDirection == '3')
+  {
+    motorForward(0, power);
+    motorBackward(1, power);
+  }
+  //Letter d/D. Right turn.
+  else if (moveDirection == '4')
+  {
+    motorBackward(0, power);
+    motorForward(1, power);
+  }
+  //Letter s/S. Rear movement
+  else if (moveDirection == '1')
+  {
+    motorBackward(0, power);
+    motorBackward(1, power);
+  }
+  //Letter q/Q. Stop movement.
+  else if (moveDirection == '5')
+  {
+    motorAllStop();
+  }
+}
