a4110435add1ebfe83332207ca507874502d843e
smiltainis <smilte.petronyte@gmail.com>
2020-02-27 10:15:15 +0000

    field path planning

--- server/pathfinding/field_path.py
+++ server/pathfinding/field_path.py
@@ -6,45 +6,47 @@ import sys
 sys.path.append("..\\..\\")
 from server.vision.camera import Camera
 from server.vision.room import Room
-from base import *
-from server.common.graphics import GraphWin, Transform
 from sympy import Polygon, Point2D
+import matplotlib.pyplot as plt
 
-class Planner:
+class FieldPlanner:
 
     KP = 5
-    ETA = 100
+    ETA = 100000000
 
-    def __init__(self, room_width, room_height):
-        self.number_of_tables = None
+    def __init__(self, room_width, room_height, obstacles, grid_size, robot_radius):
+        # self.number_of_tables = None
         # self.camera = Camera(0)
         self.room_width = room_width
         self.room_height = room_height
+        self.grid_size = grid_size
+        self.robot_radius = robot_radius
+        # self.path = []
+        self.goals = dict()
+        self.tables = dict()
+        self.paths = dict()
+        # self.xs = []
+        # self.ys = []
+        self.ox, self.oy, self.orad = self.get_static_obstacles(obstacles)
         # self.ox, self.oy, self.orad = self.get_static_obstacles(Room(0).obsts)
-        self.ox, self.oy, self.orad = self.get_static_obstacles(self.set_static_obstacles())
-        self.tables = self.set_table_centres()
-        self.goals = self.tables_to_goals(self.set_goal_coords())
-        self.plan()
-        self.path = []
-        print(self.path)
-        # self.draw()
+        # self.ox, self.oy, self.orad = self.get_static_obstacles(self.set_static_obstacles())
+        # self.plan()
+
 
     def get_table_centres(self, table_id, pos):
 
         """
-        get_table_centres: returns centre of the specified table, and the list of other table centres excluding
-        one that was specified
+        get_table_centres: returns centre coordinates and the orientation of the specified table,
+        and the list of other table centres excluding one that was specified
 
         :param table_id : id of the table that should not be included in the list
         :param pos: dictionary of table id's & ar tag positions
-        :rtype: point, [point], point
         """
 
-        # calculates the new obstacles(table_centres)
+        # lists of dynamic obstacles (other tables)
         tx = []
         ty = []
         for id in pos:
-            # centre = [x, y]
             centre = (pos[id][0] + pos[id][2]) / 2
             if id == table_id:
                 cx = centre[0]
@@ -59,15 +61,15 @@ class Planner:
     def calc_attractive_potential(self, x, y, gx, gy):
         return 0.5 * self.KP * np.hypot(x - gx, y - gy)
 
-    def calc_repulsive_potential(self, x, y, ox, oy, rr):
+    def calc_repulsive_potential(self, x, y, tx, ty):
 
-        safe_radius = rr
+        safe_radius = self.robot_radius
 
         # search nearest dynamic obstacle
         minid_d = -1
         dmin_d = float("inf")
-        for i, _ in enumerate(ox):
-            d = np.hypot(x - ox[i], y - oy[i])
+        for i, _ in enumerate(tx):
+            d = np.hypot(x - tx[i], y - ty[i])
             if dmin_d >= d:
                 dmin_d = d
                 minid_d = i
@@ -84,15 +86,15 @@ class Planner:
         # dynamic obstacle is closer
         if dmin_s >= dmin_d:
             # calc repulsive potential
-            dq = np.hypot(x - ox[minid_d], y - oy[minid_d])
+            dq = np.hypot(x - tx[minid_d], y - ty[minid_d])
             # safe distance between tables (distance between tables is the distance btw their centres)
-            safe_radius = rr * 2
+            safe_radius = self.robot_radius * 2
         # static obstacle is closer
         else:
             # calc repulsive potential
             dq = np.hypot(x - self.ox[minid_s], y - self.oy[minid_s])
             # safe distance = obstacle radius + robot radius
-            safe_radius = self.orad[minid_s] + rr
+            safe_radius = self.orad[minid_s] + self.robot_radius
 
         if dq <= (safe_radius):
             if dq <= 0.1:
@@ -101,25 +103,42 @@ class Planner:
         else:
             return 0.0
 
-    def next_cell(self, cell_x, cell_y, gx, gy, ox, oy, reso, rr):
+    def next_cell(self, cell_x, cell_y, gx, gy, ox, oy):
 
-        # adds room boundaries: table centre position can't come closer to the wall than its radius
-        grid_bound_left = int(round(rr / reso))  # number of cells the radius takes
-        grid_bound_right = int(round(self.room_width / reso - rr / reso))
-        grid_bound_top = int(round(rr / reso))
-        grid_bound_bottom = int(round(self.room_height / reso - rr / reso))
+        """
+        next_cell: returns next cell which minimizes the potential
+
+        :param cell_x, cell_y: current cell coordinates
+        :param gx, gy: goal coordinates
+        :param ox, oy: lists of dynamic obstacle coordinates
+        """
+
+        # room boundaries in terms of grid coordinates
+        # table centre position can't come closer to the wall than its radius
+        grid_bound_left = int(round(self.robot_radius / self.grid_size))  # number of cells the radius takes
+        grid_bound_right = int(round(self.room_width / self.grid_size - self.robot_radius / self.grid_size))
+        grid_bound_top = int(round(self.robot_radius / self.grid_size))
+        grid_bound_bottom = int(round(self.room_height / self.grid_size - self.robot_radius / self.grid_size))
 
         minix, miniy = -1, -1
         minuf = float("inf")
+        # check all 8 cells around the table centre
         for ix in range((cell_x - 1), (cell_x + 2)):
-            x = ix * reso
+            x = ix * self.grid_size
             for iy in range((cell_y - 1), (cell_y + 2)):
-                y = iy * reso
+                y = iy * self.grid_size
+                # the current cell should not be considered as it would result in robot staying in place
+                # however might be useful for dynamic obstacle avoidance
+                if ix == cell_x and iy == cell_y:
+                    continue
                 if ix <= grid_bound_right and ix >= grid_bound_left \
                         and iy <= grid_bound_bottom and iy >= grid_bound_top:
                     ug = self.calc_attractive_potential(x, y, gx, gy)
-                    uo = self.calc_repulsive_potential(x, y, ox, oy, rr)
+                    uo = self.calc_repulsive_potential(x, y, ox, oy)
                     uf = ug + uo
+                else:
+                    uf = float("inf")
+                # find the cell that minimizes potential
                 if minuf > uf:
                     minuf = uf
                     minix = ix
@@ -127,17 +146,15 @@ class Planner:
 
         return minix, miniy
 
-    # def move(self, id, x, y, cx, cy, orientation):
-    #
-    #     self.update_table(id, x, y, cx, cy, orientation)
-
 
     def multi_path(self):
 
-        # goals = dictionary which maps table id with its goal
+        """
+        multi_path: computes the next step for each table and moves it
 
-        reso = 10
-        rr = 115.0
+        """
+
+        # goals = dictionary which maps table id with its goal
 
         # pos = self.camera.get_pos(self.number_of_tables)
         pos = self.tables
@@ -146,37 +163,52 @@ class Planner:
             cx, cy, orientation, tx, ty = self.get_table_centres(id, pos)
             sx = cx
             sy = cy
-            ix = round(sx / reso)
-            iy = round(sy / reso)
+            ix = int(round(sx / self.grid_size))
+            iy = int(round(sy / self.grid_size))
             gx = self.goals[id][0]
             gy = self.goals[id][1]
 
             # distance to goal
             d = np.hypot(sx - gx, sy - gy)
-            if d >= reso:
-                # ox and oy represent static obstacles
+            # if robot is not in the same grid as its goal position, find the optimal gird cell
+            if d >= self.grid_size:
+                # ox and oy represent static obstacles are attributes of the class (self.ox, self.oy)
                 # tx and ty represent the centre coordinates of the tables (dynamic)
-                otx = self.ox + tx
-                oty = self.oy + ty
-                minix, miniy = self.next_cell(ix, iy, gx, gy, otx, oty, reso, rr)
-                xp = minix * reso
-                yp = miniy * reso
+                minix, miniy = self.next_cell(ix, iy, gx, gy, tx, ty)
+                xp = minix * self.grid_size
+                yp = miniy * self.grid_size
                 # move table with id
                 self.update_table(id, xp, yp, cx, cy, orientation)
-                self.path.append((xp, yp))
+                self.paths[id][0].append(xp)
+                self.paths[id][1].append(yp)
+            # robot is in the same grid as its goal position, move it to its exact goal position
             else:
-                self.path.append((gx, gy))
+                self.paths[id][0].append(gx)
+                self.paths[id][1].append(gy)
                 self.update_table(id, gx, gy, cx, cy, orientation)
-                self.goals.pop(id) # remove entry from the goals as table is in the goal position
+                # remove entry from the goals as table is in the goal position
+                self.goals.pop(id)
 
 
     def tables_to_goals(self, goals):
 
-        # TO-DO: given a list of goal positions create a dictionary which maps a table to position
+        """
+        tables_to_goals: returns a dictionary of table id to goal assignments
+
+        NOTE: at the moment table is assigned to a goal that is closest to it using euclidean distance.
+        Such assignment does not consider any obstacles and might not be optimal.
+        Once the goal has been assigned to a table, it is removed from the list of goals, so no two tables will
+        have the same goal.
+
+        :param goals: list of goal positions
+
+        """
+
         # pos = self.camera.get_pos(self.number_of_tables)
         pos = self.tables
         tables_with_goals = dict()
 
+        # iterate through tables
         for id in pos:
             mindist = float("inf")
             optimal_goal = None
@@ -184,21 +216,40 @@ class Planner:
             centre = (pos[id][0] + pos[id][2]) / 2
             for goal in goals:
                 dist = np.hypot(goal[0] - centre[0], goal[1] - centre[1])
+                # minimize the distance
                 if mindist >= dist:
                     mindist = dist
                     optimal_goal = goal
+            # assign goal to the table id
             tables_with_goals[id] = optimal_goal
             goals.remove(goal)
 
         return tables_with_goals
 
-    def plan(self):
+    def plan(self, goals):
 
+        """
+        plan: returns path for every table
+
+        :param goals: list of goal positions
+
+        """
+
+        self.goals = self.tables_to_goals(goals)
+        # while there are tables that are not at their goal positions
         while bool(self.goals):
             self.multi_path()
+        return self.paths
         print("Goal!")
 
     def get_static_obstacles(self, obstacles):
+
+        """
+        get_static_obstacles: returns the centre coordinates and the radius for all static obstacles
+
+        :param obstacles: dictionary of obstacles
+
+        """
 
         ox = []
         oy = []
@@ -206,35 +257,65 @@ class Planner:
 
         for id in obstacles:
 
-            centre = (obstacles[id][0] + obstacles[id][1]) / 2
-            ox.append(centre[0])
-            oy.append(centre[1])
-            r = np.hypot(centre[0] - obstacles[id][0][0], centre[1] - obstacles[id][0][1])
+            # centre coordinates
+            x = (obstacles[id][0][0] + obstacles[id][1][0]) / 2
+            y = (obstacles[id][0][1] + obstacles[id][1][1]) / 2
+            ox.append(x)
+            oy.append(y)
+            r = np.hypot(x - obstacles[id][0][0], y - obstacles[id][0][1])
             orad.append(r)
 
         return ox, oy, orad
 
-    #test data
-    def set_table_centres(self):
+# these are functions I have used for testing, most of them irrelevant to you
+
+    # creates a dictionary tables manually instead of using a camera
+    def set_table_centres(self, cx, cy):
+
 
         ar_diagonal = sqrt(19 * 19 * 2)
         radius = round(ar_diagonal / 2)
 
-        # ar tag for table 1
         table1_id = 1
-        center1 = (200, 230)
+        center1 = (cx, cy)
         rotation = pi / 6
         tag1 = Polygon(center1, radius, rotation, n=4)
         vertices = tag1.vertices
-        p1 = np.array([vertices[3][0], vertices[3][1]], dtype=float)
-        p2 = np.array([vertices[0][0], vertices[0][1]], dtype=float)
-        p3 = np.array([vertices[1][0], vertices[1][1]], dtype=float)
-        p4 = np.array([vertices[2][0], vertices[2][1]], dtype=float)
+        vertices = self.order_coords(vertices)
+        p1 = np.array([vertices[0][0], vertices[0][1]], dtype=float)
+        p2 = np.array([vertices[1][0], vertices[1][1]], dtype=float)
+        p3 = np.array([vertices[2][0], vertices[2][1]], dtype=float)
+        p4 = np.array([vertices[3][0], vertices[3][1]], dtype=float)
         ar1_coords = np.array([p1, p2, p3, p4])
 
         tables = {table1_id: ar1_coords}
 
+        self.tables = tables
+        self.paths[table1_id] = [[cx], [cy]]
+
         return tables
+
+    def order_coords(self, coords):
+
+        ordered_coords = []
+        miny = float("inf")
+        first = -1
+        for i in range(len(coords)):
+            x = coords[i][0]
+            y = coords[i][1]
+            if y < miny:
+                y = miny
+                first = i
+            if y == miny:
+                if x < coords[first][0]:
+                    first = i
+                    break
+        second = (first + 1) % 4
+        third = (first + 2) % 4
+        fourth = (first + 3) % 4
+        ordered_coords = [coords[first], coords[second], coords[third], coords[fourth]]
+
+        return ordered_coords
 
     def update_table(self, id, x, y, cx, cy, orientation):
 
@@ -244,17 +325,18 @@ class Planner:
         target_orientation = np.array((x - cx, y - cy))
 
         rotation = self.angleBetweenVectors(orientation, target_orientation)
-        ar_tag = Polygon(cx, cy, radius, rotation, n=4)
+        ar_tag = Polygon((x, y), radius, rotation, n=4)
         vertices = ar_tag.vertices
-        p1 = np.array([vertices[3][0], vertices[3][1]], dtype=float)
-        p2 = np.array([vertices[0][0], vertices[0][1]], dtype=float)
-        p3 = np.array([vertices[1][0], vertices[1][1]], dtype=float)
-        p4 = np.array([vertices[2][0], vertices[2][1]], dtype=float)
+        vertices = self.order_coords(vertices)
+        p1 = np.array([vertices[0][0], vertices[0][1]], dtype=float)
+        p2 = np.array([vertices[1][0], vertices[1][1]], dtype=float)
+        p3 = np.array([vertices[2][0], vertices[2][1]], dtype=float)
+        p4 = np.array([vertices[3][0], vertices[3][1]], dtype=float)
         ar_coords = np.array([p1, p2, p3, p4])
 
         self.tables[id] = ar_coords
 
-    def angleBetweenVectors(v1, v2) -> float:
+    def angleBetweenVectors(self, v1, v2) -> float:
         cos_a = np.dot(v1, v2)
         sin_a = la.norm(np.cross(v1, v2))
         return np.arctan2(sin_a, cos_a)
@@ -279,11 +361,6 @@ class Planner:
 
         goal1 = (1235, 563)  # (x, y)
         return [goal1]
-        # p = (525, 564)  #left
-        # p = (936, 296)  #top
-        # p = (1337, 551)  #right occupied now
-        # p = (977, 920)  # bottom
 
-    # def draw(self):
 if __name__ == '__main__':
-    Planner(1590, 1072)
+    FieldPlanner(1590, 1072)
---
+++ server/test_field_planner.py
@@ -0,0 +1,78 @@
+import sys
+import os
+sys.path.append(os.path.abspath(__file__ + '/../..'))
+from server.vision.room import Room
+import numpy as np
+from server.pathfinding.planner import AStarPlanner
+from server.pathfinding.field_path import FieldPlanner
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+import time
+
+
+
+
+r = Room('room0')  # Load saved room and obstacles
+# add border
+ox, oy = [], []
+
+def draw_rect(pt1, pt2):
+    # Bottom left
+    bl_x, bl_y = pt1[0], pt1[1]
+    # Top right
+    tr_x, tr_y = pt2[0], pt2[1]
+
+    # Horizontal side length
+    hside = tr_x - bl_x + 1
+    # Vertical side length
+    vside = tr_y - bl_y + 1
+
+    # Insert the coordinates to ox, oy
+    for i in range(hside):  # Top and bottom sides
+        ox.append(bl_x + i)
+        oy.append(bl_y)
+        ox.append(bl_x + i)
+        oy.append(tr_y)
+    for j in range(vside):
+        ox.append(bl_x)
+        oy.append(bl_y + j)
+        ox.append(tr_x)
+        oy.append(bl_y + j)
+
+
+
+for pt1, pt2 in r.obsts.values():
+    draw_rect(pt1, pt2)
+# start, goal
+sx, sy, gx, gy = 460.0, 530.0, 1430.0, 800.0
+
+
+grid_size = 32.0
+robot_radius = 130.0
+
+###
+plt.imshow(r.frame_orig)
+plt.plot(ox, oy, ".k")
+plt.plot(sx, sy, "og")
+plt.plot(gx, gy, "xb")
+plt.grid(False)
+plt.axis("equal")
+
+obstacles = r.obsts
+room_width, room_height = obstacles[0][1]
+obstacles.pop(0) #remove room bounds
+potential_field = FieldPlanner(room_width, room_height, obstacles, grid_size, robot_radius)
+potential_field.set_table_centres(460.0, 530.0)
+paths = potential_field.plan([(gx, gy)])
+rx, ry = paths[1]
+
+ax = plt.gca()
+
+for i in range(4):
+    plt.plot(potential_field.ox[i], potential_field.oy[i], marker="*", color="gold")
+radius = potential_field.orad
+for i in range(len(radius)):
+    circle = Circle((potential_field.ox[i], potential_field.oy[i]), radius[i], color="gold", fill=False)
+    ax.add_patch(circle)
+plt.plot(rx, ry, "-r")
+plt.show()
